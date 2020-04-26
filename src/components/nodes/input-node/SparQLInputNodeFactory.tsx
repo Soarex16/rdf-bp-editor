6884f9dbacf82ce3639b6f3f21826ef8907016ef
Soarex16 <soarex16@gmail.com>
2020-04-26 16:05:23 +0300

    Move todo's from code to gh issues

--- src/components/nodes/input-node/SparQLInputNodeFactory.tsx
+++ src/components/nodes/input-node/SparQLInputNodeFactory.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import {SparQLInputNodeModel, sparQLInputNodeType} from './SparQLInputNodeModel';
-import {AbstractReactFactory, GenerateWidgetEvent, ListenerHandle} from '@projectstorm/react-canvas-core';
+import {AbstractReactFactory, GenerateWidgetEvent} from '@projectstorm/react-canvas-core';
 import {DiagramEngine} from '@projectstorm/react-diagrams-core';
 import {SparQLInputNodeDiagramWidget} from './SparQLInputNodeWidget';
 
 export class SparQLInputNodeFactory extends AbstractReactFactory<SparQLInputNodeModel, DiagramEngine> {
-    // save the event handle to register it only once
-    updateLinkEventHandle: ListenerHandle;
-
     constructor() {
         super(sparQLInputNodeType);
     }
@@ -17,26 +14,6 @@ export class SparQLInputNodeFactory extends AbstractReactFactory<SparQLInputNode
     }
 
     generateReactWidget(event: GenerateWidgetEvent<SparQLInputNodeModel>): JSX.Element {
-        // TODO:
-        // automatically set FlowLinkLabel alias equal to result set alias
-        /*if (!this.updateLinkEventHandle) {
-            // event: BaseEntityEvent & { link: LinkModel; isCreated: boolean; }
-            const updateLinkLabel = (ev: any) => {
-                if (ev.isCreated) {
-                    const link = ev.link as BranchLinkModel;
-
-                    if (ev.link.getSourcePort().getNode().getType() === 'sparql-input-node') {
-                        const sourceNode: SparQLInputNodeModel = link.getSourcePort().getNode() as SparQLInputNodeModel;
-                        (link.getLabels()[0] as TextInputLabelModel).formula = `\\text{${sourceNode.resultSetAlias}}`;
-                    }
-                }
-            };
-
-            this.updateLinkEventHandle = this.engine.getModel().registerListener({
-                'linksUpdated': updateLinkLabel
-            });
-        }*/
-
         return <SparQLInputNodeDiagramWidget engine={this.engine as DiagramEngine} node={event.model}/>;
     }
 
